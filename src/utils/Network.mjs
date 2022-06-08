939643c70ae078f4199e2918b35b32e893299609
KeKs0r <marc.hoeffl@laserfocus.io>
2022-06-08 08:23:29 +0200

    Add possibility to check for delete logic

--- src/utils/Network.mjs
+++ src/utils/Network.mjs
@@ -1,85 +1,95 @@
-import _ from 'lodash'
-import { multiply, pow, format, bignumber } from 'mathjs'
-import QueryClient from './QueryClient.mjs'
-import SigningClient from './SigningClient.mjs'
-import Operator from './Operator.mjs'
-import Chain from './Chain.mjs'
-import CosmosDirectory from './CosmosDirectory.mjs'
+import _ from "lodash";
+import { multiply, pow, format, bignumber } from "mathjs";
+import QueryClient from "./QueryClient.mjs";
+import SigningClient from "./SigningClient.mjs";
+import Operator from "./Operator.mjs";
+import Chain from "./Chain.mjs";
+import CosmosDirectory from "./CosmosDirectory.mjs";
 
 class Network {
   constructor(data) {
-    this.data = data
-    this.enabled = data.enabled
-    this.experimental = data.experimental
-    this.apyEnabled = data.apyEnabled
-    this.name = data.path || data.name
-    this.default = data.default
-    this.directory = CosmosDirectory()
+    this.data = data;
+    this.enabled = data.enabled;
+    this.experimental = data.experimental;
+    this.apyEnabled = data.apyEnabled;
+    this.name = data.path || data.name;
+    this.default = data.default;
+    this.directory = CosmosDirectory();
 
-    this.rpcUrl = this.directory.rpcUrl(this.name) // only used for Keplr suggestChain
-    this.restUrl = data.restUrl || this.directory.restUrl(this.name)
+    this.rpcUrl = this.directory.rpcUrl(this.name); // only used for Keplr suggestChain
+    this.restUrl = data.restUrl || this.directory.restUrl(this.name);
 
-    this.usingDirectory = !![this.restUrl].find(el => {
-      const match = el => el.match("cosmos.directory")
+    this.usingDirectory = !![this.restUrl].find((el) => {
+      const match = (el) => el.match("cosmos.directory");
       if (Array.isArray(el)) {
-        return el.find(match)
+        return el.find(match);
       } else {
-        return match(el)
+        return match(el);
       }
-    })
+    });
   }
 
   connectedDirectory() {
-    const apis = this.chain ? this.chain.chainData['best_apis'] : this.data['best_apis']
-    return apis && ['rest'].every(type => apis[type].length > 0)
+    const apis = this.chain
+      ? this.chain.chainData["best_apis"]
+      : this.data["best_apis"];
+    return apis && ["rest"].every((type) => apis[type].length > 0);
   }
 
   async load() {
-    this.chain = await Chain(this.data)
-    this.validators = await this.directory.getValidators(this.name)
-    this.operators = (this.data.operators || this.validators.filter(el => el.restake)).map(el => {
-      return Operator(el)
-    })
-    this.prettyName = this.chain.prettyName
-    this.chainId = this.chain.chainId
-    this.prefix = this.chain.prefix
-    this.slip44 = this.chain.slip44
-    this.denom = this.chain.denom
-    this.symbol = this.chain.symbol
-    this.decimals = this.chain.decimals
-    this.image = this.chain.image
-    this.coinGeckoId = this.chain.coinGeckoId
-    this.estimatedApr = this.chain.estimatedApr
-    this.authzSupport = this.chain.authzSupport
-    const defaultGasPrice = format(bignumber(multiply(0.000000025, pow(10, this.decimals))), { notation: 'fixed' }) + this.denom
-    this.gasPrice = this.data.gasPrice || defaultGasPrice
-    this.gasPriceStep = this.data.gasPriceStep
-    this.gasPricePrefer = this.data.gasPricePrefer
-    this.gasModifier = this.data.gasModifier || 1.3
-    this.txTimeout = this.data.txTimeout || 60_000
+    this.chain = await Chain(this.data);
+    this.validators = await this.directory.getValidators(this.name);
+    this.operators = (
+      this.data.operators || this.validators.filter((el) => el.restake)
+    ).map((el) => {
+      return Operator(el);
+    });
+    this.prettyName = this.chain.prettyName;
+    this.chainId = this.chain.chainId;
+    this.prefix = this.chain.prefix;
+    this.slip44 = this.chain.slip44;
+    this.denom = this.chain.denom;
+    this.symbol = this.chain.symbol;
+    this.decimals = this.chain.decimals;
+    this.image = this.chain.image;
+    this.coinGeckoId = this.chain.coinGeckoId;
+    this.estimatedApr = this.chain.estimatedApr;
+    this.authzSupport = this.chain.authzSupport;
+    const defaultGasPrice =
+      format(bignumber(multiply(0.000000025, pow(10, this.decimals))), {
+        notation: "fixed",
+      }) + this.denom;
+    this.gasPrice = this.data.gasPrice || defaultGasPrice;
+    this.gasPriceStep = this.data.gasPriceStep;
+    this.gasPricePrefer = this.data.gasPricePrefer;
+    this.gasModifier = this.data.gasModifier || 1.3;
+    this.txTimeout = this.data.txTimeout || 60_000;
   }
 
   async connect() {
     try {
-      this.queryClient = await QueryClient(this.chain.chainId, this.restUrl)
-      this.restUrl = this.queryClient.restUrl
-      this.connected = this.queryClient.connected && (!this.usingDirectory || this.connectedDirectory())
+      this.queryClient = await QueryClient(this.chain.chainId, this.restUrl);
+      this.restUrl = this.queryClient.restUrl;
+      this.connected =
+        this.queryClient.connected &&
+        (!this.usingDirectory || this.connectedDirectory());
     } catch (error) {
-      console.log(error)
-      this.connected = false
+      console.log(error);
+      this.connected = false;
     }
   }
 
-  async getApy(validators, operators){
-    const chainApr = this.chain.estimatedApr
+  async getApy(validators, operators) {
+    const chainApr = this.chain.estimatedApr;
     let validatorApy = {};
     for (const [address, validator] of Object.entries(validators)) {
-      if(validator.jailed || validator.status !== 'BOND_STATUS_BONDED'){
-        validatorApy[address] = 0
-      }else{
-        const commission = validator.commission.commission_rates.rate
-        const operator = operators.find((el) => el.address === address)
-        const periodPerYear = operator && this.chain.authzSupport ? operator.runsPerDay() * 365 : 1;
+      if (validator.jailed || validator.status !== "BOND_STATUS_BONDED") {
+        validatorApy[address] = 0;
+      } else {
+        const commission = validator.commission.commission_rates.rate;
+        const operator = operators.find((el) => el.address === address);
+        const periodPerYear =
+          operator && this.chain.authzSupport ? operator.runsPerDay() * 365 : 1;
         const realApr = chainApr * (1 - commission);
         const apy = (1 + realApr / periodPerYear) ** periodPerYear - 1;
         validatorApy[address] = apy;
@@ -89,42 +99,42 @@ class Network {
   }
 
   signingClient(wallet, key, gasPrice) {
-    if (!this.queryClient)
-      return
+    if (!this.queryClient) return;
 
-    return SigningClient(this, gasPrice || this.gasPrice, wallet, key)
+    return SigningClient(this, gasPrice || this.gasPrice, wallet, key);
   }
 
   getOperator(operatorAddress) {
-    return this.operators.find(elem => elem.address === operatorAddress)
+    return this.operators.find((elem) => elem.address === operatorAddress);
   }
 
   getOperatorByBotAddress(botAddress) {
-    return this.operators.find(elem => elem.botAddress === botAddress)
+    console.log(this.operators.find((a) => a.moniker.includes("Frens")));
+    return this.operators.find((elem) => elem.botAddress === botAddress);
   }
 
   getOperators() {
-    return this.sortOperators()
+    return this.sortOperators();
   }
 
   sortOperators() {
-    const random = _.shuffle(this.operators)
+    const random = _.shuffle(this.operators);
     if (this.data.ownerAddress) {
-      return _.sortBy(random, ({ address }) => address === this.data.ownerAddress ? 0 : 1)
+      return _.sortBy(random, ({ address }) =>
+        address === this.data.ownerAddress ? 0 : 1
+      );
     }
-    return random
+    return random;
   }
 
   getValidators(opts) {
-    opts = opts || {}
-    return this.validators.filter(validator => {
-      if (opts.status)
-        return validator.status === opts.status
-      return true
-    }).reduce(
-      (a, v) => ({ ...a, [v.operator_address]: v }),
-      {}
-    )
+    opts = opts || {};
+    return this.validators
+      .filter((validator) => {
+        if (opts.status) return validator.status === opts.status;
+        return true;
+      })
+      .reduce((a, v) => ({ ...a, [v.operator_address]: v }), {});
   }
 }
 
--- src/utils/SigningClient.mjs
+++ src/utils/SigningClient.mjs
@@ -1,6 +1,6 @@
-import _ from 'lodash'
-import axios from 'axios'
-import { multiply, ceil, bignumber } from 'mathjs'
+import _ from "lodash";
+import axios from "axios";
+import { multiply, ceil, bignumber } from "mathjs";
 import Long from "long";
 
 import {
@@ -19,16 +19,25 @@ import {
 import { sleep } from "@cosmjs/utils";
 import { makeSignDoc, Registry } from "@cosmjs/proto-signing";
 import { makeSignDoc as makeAminoSignDoc } from "@cosmjs/amino";
-import { toBase64, fromBase64 } from '@cosmjs/encoding'
+import { toBase64, fromBase64 } from "@cosmjs/encoding";
 import { PubKey } from "cosmjs-types/cosmos/crypto/secp256k1/keys.js";
 import { SignMode } from "cosmjs-types/cosmos/tx/signing/v1beta1/signing.js";
-import { AuthInfo, Fee, TxBody, TxRaw } from "cosmjs-types/cosmos/tx/v1beta1/tx.js";
+import {
+  AuthInfo,
+  Fee,
+  TxBody,
+  TxRaw,
+} from "cosmjs-types/cosmos/tx/v1beta1/tx.js";
 
-import { coin } from './Helpers.mjs'
+import { coin } from "./Helpers.mjs";
 
 async function SigningClient(network, defaultGasPrice, signer, key) {
-
-  const { restUrl, gasModifier: defaultGasModifier, slip44: coinType, chainId } = network
+  const {
+    restUrl,
+    gasModifier: defaultGasModifier,
+    slip44: coinType,
+    chainId,
+  } = network;
 
   const registry = new Registry(defaultStargateTypes);
   const aminoTypes = new AminoTypes({
@@ -39,7 +48,7 @@ async function SigningClient(network, defaultGasPrice, signer, key) {
     ...createStakingAminoConverters(network.prefix),
     ...createIbcAminoConverters(),
     ...createFreegrantAminoConverters(),
-  })
+  });
 
   function getAccount(address) {
     return axios
@@ -72,148 +81,189 @@ async function SigningClient(network, defaultGasPrice, signer, key) {
             value = baseAccount;
           }
         }
-        return value 
-      })
-  };
+        return value;
+      });
+  }
 
   function getIsNanoLedger() {
-    if(!key) return false
+    if (!key) return false;
     return key.isNanoLedger;
   }
 
   // vendored to handle large integers
   // https://github.com/cosmos/cosmjs/blob/0f0c9d8a754cbf01e17acf51d3f2dbdeaae60757/packages/stargate/src/fee.ts
-  function calculateFee(gasLimit, gasPrice) {
-    const processedGasPrice = typeof gasPrice === "string" ? GasPrice.fromString(gasPrice) : gasPrice;
+  function calculateFee(gasLimit, gasPrice, defaultFee) {
+    const processedGasPrice =
+      typeof gasPrice === "string" ? GasPrice.fromString(gasPrice) : gasPrice;
     const { denom, amount: gasPriceAmount } = processedGasPrice;
-    const amount = ceil(bignumber(multiply(bignumber(gasPriceAmount.toString()), bignumber(gasLimit.toString()))));
+    const amount = ceil(
+      bignumber(
+        multiply(
+          bignumber(gasPriceAmount.toString()),
+          bignumber(gasLimit.toString())
+        )
+      )
+    );
     return {
-      amount: [coin(amount, denom)],
+      amount: defaultFee ? [coin(defaultFee, denom)] : [coin(amount, denom)],
       gas: gasLimit.toString(),
-      gasLimit: gasLimit.toString()
+      gasLimit: gasLimit.toString(),
     };
   }
 
-  function getFee(gas, gasPrice) {
-    if (!gas)
-      gas = 200000;
-    return calculateFee(gas, gasPrice || defaultGasPrice);
+  function getFee(gas, gasPrice, defaultAmount) {
+    if (!gas) gas = 200000;
+    return calculateFee(gas, gasPrice || defaultGasPrice, defaultAmount);
   }
 
-  async function signAndBroadcastWithoutBalanceCheck(address, msgs, gas, memo, gasPrice) {
+  async function signAndBroadcastWithoutBalanceCheck(
+    address,
+    msgs,
+    gas,
+    memo,
+    gasPrice
+  ) {
     const defaultOptions = _.clone(signer.keplr.defaultOptions);
     _.merge(signer.keplr.defaultOptions, {
-      sign: { disableBalanceCheck: true }
+      sign: { disableBalanceCheck: true },
     });
     try {
-      return await signAndBroadcast(address, msgs, gas, memo, gasPrice)
+      return await signAndBroadcast(address, msgs, gas, memo, gasPrice);
     } finally {
-      signer.keplr.defaultOptions = defaultOptions
+      signer.keplr.defaultOptions = defaultOptions;
     }
   }
 
-  async function signAndBroadcast(address, messages, gas, memo, gasPrice) {
-    if (!gas)
-      gas = await simulate(address, messages, memo);
-    const fee = getFee(gas, gasPrice);
-    const txBody = await sign(address, messages, memo, fee)
-    return broadcast(txBody)
+  async function signAndBroadcast(
+    address,
+    messages,
+    gas,
+    memo,
+    gasPrice,
+    defaultFee
+  ) {
+    if (!gas) gas = await simulate(address, messages, memo);
+    const fee = getFee(gas, gasPrice, defaultFee);
+    const txBody = await sign(address, messages, memo, fee);
+    return broadcast(txBody);
   }
 
-  async function broadcast(txBody){
-    const timeoutMs = network.txTimeout || 60_000
-    const pollIntervalMs = 3_000
-    let timedOut = false
+  async function broadcast(txBody) {
+    const timeoutMs = network.txTimeout || 60_000;
+    const pollIntervalMs = 3_000;
+    let timedOut = false;
     const txPollTimeout = setTimeout(() => {
       timedOut = true;
     }, timeoutMs);
-    
+
     const pollForTx = async (txId) => {
       if (timedOut) {
         throw new Error(
-          `Transaction with ID ${txId} was submitted but was not yet found on the chain. You might want to check later. There was a wait of ${timeoutMs / 1000} seconds.`
+          `Transaction with ID ${txId} was submitted but was not yet found on the chain. You might want to check later. There was a wait of ${
+            timeoutMs / 1000
+          } seconds.`
         );
       }
       await sleep(pollIntervalMs);
       try {
-        const response = await axios.get(restUrl + '/cosmos/tx/v1beta1/txs/' + txId);
-        const result = parseTxResult(response.data.tx_response)
-        return result
+        const response = await axios.get(
+          restUrl + "/cosmos/tx/v1beta1/txs/" + txId
+        );
+        const result = parseTxResult(response.data.tx_response);
+        return result;
       } catch {
         return pollForTx(txId);
       }
     };
 
-    const response = await axios.post(restUrl + '/cosmos/tx/v1beta1/txs', {
+    const response = await axios.post(restUrl + "/cosmos/tx/v1beta1/txs", {
       tx_bytes: toBase64(TxRaw.encode(txBody).finish()),
-      mode: "BROADCAST_MODE_SYNC"
-    })
-    const result = parseTxResult(response.data.tx_response)
-    assertIsDeliverTxSuccess(result)
+      mode: "BROADCAST_MODE_SYNC",
+    });
+    const result = parseTxResult(response.data.tx_response);
+    assertIsDeliverTxSuccess(result);
     return new Promise((resolve, reject) =>
       pollForTx(result.transactionHash).then(
         (value) => {
           clearTimeout(txPollTimeout);
-          assertIsDeliverTxSuccess(value)
+          assertIsDeliverTxSuccess(value);
           resolve(value);
         },
         (error) => {
           clearTimeout(txPollTimeout);
           reject(error);
-        },
-      ),
+        }
+      )
     );
   }
 
-  async function sign(address, messages, memo, fee){
-    const account = await getAccount(address)
-    const { account_number: accountNumber, sequence } = account
-    const txBodyBytes = makeBodyBytes(messages, memo)
-    if(getIsNanoLedger()){
+  async function sign(address, messages, memo, fee) {
+    const account = await getAccount(address);
+    const { account_number: accountNumber, sequence } = account;
+    const txBodyBytes = makeBodyBytes(messages, memo);
+    if (getIsNanoLedger()) {
       // Convert to amino for ledger devices
-      const aminoMsgs = messages.map(el => aminoTypes.toAmino(el))
-      const signDoc = makeAminoSignDoc(aminoMsgs, fee, chainId, memo, accountNumber, sequence);
+      const aminoMsgs = messages.map((el) => aminoTypes.toAmino(el));
+      const signDoc = makeAminoSignDoc(
+        aminoMsgs,
+        fee,
+        chainId,
+        memo,
+        accountNumber,
+        sequence
+      );
       const { signature, signed } = await signer.sign(address, signDoc);
       const authInfoBytes = await makeAuthInfoBytes(account, {
         amount: signed.fee.amount,
         gasLimit: signed.fee.gas,
-      })
+      });
       return {
         bodyBytes: txBodyBytes,
         authInfoBytes: authInfoBytes,
         signatures: [Buffer.from(signature.signature, "base64")],
-      }
-    }else{
+      };
+    } else {
       // Sign using standard protobuf messages
       const authInfoBytes = await makeAuthInfoBytes(account, {
         amount: fee.amount,
         gasLimit: fee.gas,
-      })
-      const signDoc = makeSignDoc(txBodyBytes, authInfoBytes, chainId, accountNumber);
+      });
+      const signDoc = makeSignDoc(
+        txBodyBytes,
+        authInfoBytes,
+        chainId,
+        accountNumber
+      );
       const { signature, signed } = await signer.signDirect(address, signDoc);
       return {
         bodyBytes: signed.bodyBytes,
         authInfoBytes: signed.authInfoBytes,
         signatures: [fromBase64(signature.signature)],
-      }
+      };
     }
   }
 
   async function simulate(address, messages, memo, modifier) {
-    const account = await getAccount(address)
+    const account = await getAccount(address);
     const txBody = {
       bodyBytes: makeBodyBytes(messages, memo),
-      authInfoBytes: await makeAuthInfoBytes(account, {}, SignMode.SIGN_MODE_UNSPECIFIED),
+      authInfoBytes: await makeAuthInfoBytes(
+        account,
+        {},
+        SignMode.SIGN_MODE_UNSPECIFIED
+      ),
       signatures: [new Uint8Array()],
-    }
+    };
 
-    const estimate = await axios.post(restUrl + '/cosmos/tx/v1beta1/simulate', {
-      tx_bytes: toBase64(TxRaw.encode(txBody).finish()),
-    }).then(el => el.data.gas_info.gas_used)
-    return (parseInt(estimate * (modifier || defaultGasModifier)));
+    const estimate = await axios
+      .post(restUrl + "/cosmos/tx/v1beta1/simulate", {
+        tx_bytes: toBase64(TxRaw.encode(txBody).finish()),
+      })
+      .then((el) => el.data.gas_info.gas_used);
+    return parseInt(estimate * (modifier || defaultGasModifier));
   }
 
-  function parseTxResult(result){
+  function parseTxResult(result) {
     return {
       code: result.code,
       height: result.height,
@@ -221,23 +271,26 @@ async function SigningClient(network, defaultGasPrice, signer, key) {
       transactionHash: result.txhash,
       gasUsed: result.gas_used,
       gasWanted: result.gas_wanted,
-    }
+    };
   }
 
-  function makeBodyBytes(messages, memo){
+  function makeBodyBytes(messages, memo) {
     const anyMsgs = messages.map((m) => registry.encodeAsAny(m));
     return TxBody.encode(
       TxBody.fromPartial({
         messages: anyMsgs,
         memo: memo,
       })
-    ).finish()
+    ).finish();
   }
 
-  async function makeAuthInfoBytes(account, fee, mode){
-    mode = mode || getIsNanoLedger() ? SignMode.SIGN_MODE_LEGACY_AMINO_JSON : SignMode.SIGN_MODE_DIRECT
-    const { sequence } = account
-    const accountFromSigner = (await signer.getAccounts())[0]
+  async function makeAuthInfoBytes(account, fee, mode) {
+    mode =
+      mode || getIsNanoLedger()
+        ? SignMode.SIGN_MODE_LEGACY_AMINO_JSON
+        : SignMode.SIGN_MODE_DIRECT;
+    const { sequence } = account;
+    const accountFromSigner = (await signer.getAccounts())[0];
     if (!accountFromSigner) {
       throw new Error("Failed to retrieve account from signer");
     }
@@ -259,7 +312,7 @@ async function SigningClient(network, defaultGasPrice, signer, key) {
         },
       ],
       fee: Fee.fromPartial(fee),
-    }).finish()
+    }).finish();
   }
 
   return {
@@ -268,7 +321,7 @@ async function SigningClient(network, defaultGasPrice, signer, key) {
     getIsNanoLedger,
     simulate,
     signAndBroadcast,
-    signAndBroadcastWithoutBalanceCheck
+    signAndBroadcastWithoutBalanceCheck,
   };
 }
 
